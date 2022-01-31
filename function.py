843db714b337c6b02c9083aed98925ddea1bfdf3
BatteryJuicy <50225219+BatteryJuicy@users.noreply.github.com>
2022-01-31 22:25:34 +0200

    Update function.py
    With the pyplot module it can graph an equation(quadratic or linear) using the graph() function which takes as input a list of tuples of length 2(aka a list of 2D points)

--- function.py
+++ function.py
@@ -1,36 +1,46 @@
 from math import sqrt
+import matplotlib.pyplot as plt
 
-def func(equation):
+def func(equation, a, b, c, def_set):
+    """(type of function(quadratic or linear), a, b, c, distance of O(0,0) and x (±))"""
     points_set = []
-    def_set = int(input("absolute range of x values to compute: "))
-    x = def_set
     if equation.lower() == "linear":
-        a = float(input("tangent of the line: "))
-        b = float(input("displacement:"))
+        print("the formula is: f(x) = a*x + b")
 
-        for i in range(-def_set, def_set):
+        for i in range(-def_set, def_set + 1):
             points_set.append((i, a * i + b))
         print("intersects with x'x at (" + str(-b/a) + ",0)")
         print("intersects with y'y at (0," + str(b) + ")")
+
     elif equation.lower() == "quadratic":
-        print("the formula is: f(x) = a(x - b)^2 + c")
-        a = float(input("(a) factor: "))
-        b = float(input("(b) horizontal displacement: "))
-        c = float(input("(c) Vertical displacement: "))
+        print("the formula is: f(x) = a*(x - b)^2 + c")
         
-        for i in range(-def_set, def_set):
-            points_set.append((i, a(i - b)**2 + c))
+        for i in range(-def_set, def_set + 1):
+            points_set.append((i, a*(i - b)**2 + c))
+
         d = b**2 - 4*a*c
-        if d >= 0:
+        if d > 0:
             x_intersection1 = (sqrt(d) - b) / (2*a)
             x_intersection2 = (-sqrt(d) - b) / (2*a)
             print("intersects with x'x at (", str(x_intersection1) + ",0)" + "and at (" + str(x_intersection2) + ",0)")
+        elif d == 0:
+            x_intersection = (sqrt(d) - b) / (2*a)
+            print("touches x'x at (", str(x_intersection) + ",0)")
         else:
             print("no intersection point with x'x")
         print("intersects with y'y at (0," + str(a*(b**2) + c) + ")")
-    else:
-        print("The only equations available are: 'linear', 'quadratic'.")
-    print(points_set)
+    return(points_set)
 
-    
-func("quadratic")
+def graph(points_list):
+    """graphs the points of the function specified using the func() method."""
+    def cords(lst, cord):
+        result = []
+        for i in range(len(lst)):
+            result.append(lst[i][cord])
+        return result
+
+    x_cords = cords(points_list, 0)
+    y_cords = cords(points_list, 1)
+
+    plt.plot(x_cords, y_cords)
+    plt.show()
